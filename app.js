41892aebb735abeaf2cbf9eda501e041094c0211
Soro <soroferelaha01@gmail.com>
2025-07-30 10:31:39 +0100

    NavBar animé

--- app.js
+++ app.js
@@ -3,18 +3,32 @@ const modaleEquipement = document.querySelector('.bloc-modale');
 const fermertureModale = document.querySelector('.fermerture-modale');
 const imgIndex = document.querySelector('.bloc-modale img');
 
-btns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
 
-        imgIndex.src = `ressources/${e.target.getAttribute("data-index")}.jpg`;
-        modaleEquipement.classList.add('active-modale');
+if(window.matchMedia("(min-width: 850px)").matches){
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
 
+            imgIndex.src = `ressources/${e.target.getAttribute("data-index")}.jpg`;
+            modaleEquipement.classList.add('active-modale');
+
+        })
     })
-})
 
-fermertureModale.addEventListener('click', () => {
-    modaleEquipement.classList.remove('active-modale');
-})
-modaleEquipement.addEventListener('click', () => {
-    modaleEquipement.classList.remove('active-modale');
+    modaleEquipement.addEventListener('click', () => {
+        modaleEquipement.classList.remove('active-modale');
+    })
+}
+
+
+// Anim Navbar
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    
+    if(window.scrollY > 30) {
+        nav.classList.add('anim-nav');
+    } else {
+        nav.classList.remove('anim-nav');
+    }
 })
